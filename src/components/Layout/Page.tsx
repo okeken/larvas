9a278aaf9a5464a21b2f1e88de5babebc921de61
okeken <okekehinde@gmail.com>
2022-01-28 18:59:38 +0100

    build resolved

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'contexts/Localization'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { DEFAULT_META, getCustomMeta } from 'config/constants/meta'
-//import { useCakeBusdPrice } from 'hooks/useBUSDPrice'
+// import { useCakeBusdPrice } from 'hooks/useBUSDPrice'
 import Container from './Container'
 
 const StyledPage = styled(Container)`
@@ -26,7 +26,7 @@ const StyledPage = styled(Container)`
 export const PageMeta: React.FC<{ symbol?: string }> = ({ symbol }) => {
   const { t } = useTranslation()
   const { pathname } = useRouter()
-  //const cakePriceUsd = useCakeBusdPrice()
+  // const cakePriceUsd = useCakeBusdPrice()
  // const cakePriceUsdDisplay = cakePriceUsd ? `$${cakePriceUsd.toFixed(3)}` : '...'
 
   const pageMeta = getCustomMeta(pathname, t) || {}
